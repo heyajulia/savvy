245c7b1e74e8a0cc00898f57ccc80dcc6b5feeb8
Julia <juliariety@gmail.com>
2023-03-30 11:37:52 +0000

    more wip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { EnergyPrices } from "./types.ts";
+import { Prices, ZodApiResponse } from "./types.ts";
 import addCharges from "./add-charges.ts";
 import prepareQueryParameters from "./prepare-query-parameters.ts";
 
 import "dotenv/load.ts";
-import { DateTime } from "https://cdn.skypack.dev/luxon@3.3.0?dts";
+import { DateTime } from "luxon";
 
 async function main() {
   const prices = await getEnergyPrices();
@@ -11,33 +11,22 @@ async function main() {
   console.log(prices);
 }
 
-async function getEnergyPrices(): Promise<MyPrices> {
+async function getEnergyPrices(): Promise<Prices> {
   const parameters = prepareQueryParameters();
   const response = await fetch(
     `https://api.energyzero.nl/v1/energyprices?${parameters}`,
   );
-  const prices = await response.json() as EnergyPrices;
+  const { Prices: prices, average, tillDate } = ZodApiResponse.parse(
+    await response.json(),
+  );
 
-  prices.average = addCharges(prices.average);
-
-  for (const price of prices.Prices) {
-    price.price = addCharges(price.price);
-  }
-
-  const myPrices = {} as MyPrices;
-
-  myPrices.prices = prices.Prices.map(({ price }) => price);
-  myPrices.average = prices.average;
-  myPrices.date = DateTime.fromISO(prices.tillDate, { locale: "nl" })
-    .toLocaleString(DateTime.DATE_HUGE);
-
-  return myPrices;
-}
-
-interface MyPrices {
-  prices: number[];
-  average: number;
-  date: string;
+  return {
+    prices: prices.map(({ price }) => addCharges(price)),
+    average: addCharges(average),
+    date: DateTime.fromISO(tillDate, { locale: "nl" }).toLocaleString(
+      DateTime.DATE_HUGE,
+    ),
+  };
 }
 
 main();
