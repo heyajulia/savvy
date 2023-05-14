f9acf5cf16e428c371d065c371c571089e4e0604
Julia <juliariety@gmail.com>
2023-05-14 21:42:59 +0000

    gofmt range.go

--- internal/ranges/range.go
+++ internal/ranges/range.go
@@ -23,7 +23,7 @@ func New(start int, end int) Range {
 	if start < 0 || end < 0 {
 		panic("ranges.New: start and end must be positive")
 	}
-	
+
 	return Range{start: start, end: end}
 }
 
@@ -62,21 +62,7 @@ func Collapse(values []int) []Range {
 
 func Format(ranges []Range) string {
 	rs := fp.Map(func(r Range) string {
-		sverb := "%02d"
-		everb := "%02d"
-
-		// If start or end is negative, the minus sign already takes up one character, so we need to add one to the verb
-		// I don't know if this is worth guarding against since negative hours should never occur in practice
-
-		if r.start < 0 {
-			sverb = "%03d"
-		}
-
-		if r.end < 0 {
-			everb = "%03d"
-		}
-
-		return fmt.Sprintf(fmt.Sprintf("van %s:00 tot %s:59", sverb, everb), r.start, r.end)
+		return fmt.Sprintf("van %02d:00 tot %02d:59", r.start, r.end)
 	}, ranges)
 
 	s := strings.Join(rs, ", ")
