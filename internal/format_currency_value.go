46816f78a04eeedfdf8b5d0eb3ce9fd6bc08c6c7
Julia <juliariety@gmail.com>
2024-07-03 15:31:10 +0000

    Don't display leading minus if the price is zero.
    For some reason, comparing v against math.Copysign(0, -1) passes the
    tests but still displays the leading minus in the message text. This
    code passes the tests and seems to work as expected in practice.
    
    What can I say? Floating-point numbers are weird.

--- internal/format_currency_value.go
+++ internal/format_currency_value.go
@@ -2,9 +2,14 @@ package internal
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
 func FormatCurrencyValue(value float64) string {
-	return strings.Replace(fmt.Sprintf("€\u00a0%.2f", value), ".", ",", 1)
+	v := math.Round(value*100) / 100
+	if v == 0 && math.Signbit(v) { // is v negative zero?
+		v = 0
+	}
+	return strings.Replace(fmt.Sprintf("€\u00a0%.2f", v), ".", ",", 1)
 }
