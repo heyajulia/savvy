0413561a26043e7167e5e195762149cc9a1c8481
Julia <juliariety@gmail.com>
2024-07-06 18:10:10 +0000

    Implement "start" and "privacy" commands.
    Doing this took a significant refactor of the code. Until now, we always
    assumed we would send one message per day and then immediately exit
    until we were woken up again. This worked fine for a very long time, but
    new Telegram policies[0] require the bot to provide users with a privacy
    policy.
    
    Because the bot no longer exits immediately, we don't need the systemd
    service and timer files anymore.
    
    There are still some TODOs left in the code, and the README still needs
    to be updated.
    
    [0]: https://t.me/BotNews/96

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -25,21 +25,13 @@ import (
 )
 
 type credentials struct {
-	Telegram   string `json:"telegram"`
-	MonitorURL string `json:"cronitor_url,omitempty"`
+	Telegram string `json:"telegram"`
 }
 
 var (
-	dryRun, showVersion       bool
-	token, monitorURL, chatID string
-)
-
-type state int
-
-const (
-	stateRun state = iota + 1
-	stateComplete
-	stateFail
+	dryRun, showVersion   bool
+	token, chatID         string
+	lastProcessedUpdateID uint64
 )
 
 func init() {
@@ -82,16 +74,12 @@ func init() {
 	if token != "" {
 		log.Warn("using token from flag. do not use this flag in production.")
 
-		// As a consequence of exiting here, Cronitor will not be notified of the state of the job. That's fine, though;
-		// this flag is only for local testing.
-
 		return
 	}
 
 	creds := readCredentials(log)
 
 	token = creds.Telegram
-	monitorURL = creds.MonitorURL
 }
 
 func readCredentials(log *slog.Logger) credentials {
@@ -128,24 +116,103 @@ func main() {
 	log := slog.Default()
 
 	if dryRun {
-		log.Warn("dry run mode is enabled; will not send messages or ping Cronitor")
+		log.Warn("dry run mode is enabled; will not send messages")
 	}
-
-	pingCronitor(log, stateRun)
 
 	u, err := user.Current()
 	if err != nil {
 		log.Error("could not get current user", slog.Any("err", err))
-		pingCronitor(log, stateFail)
 		os.Exit(1)
 	}
 
 	log.Info("user information", slog.Group("user", slog.String("name", u.Username), slog.String("uid", u.Uid)))
 
+	// TODO: I don't think it matters much in this case, but we could refactor this to use channels and goroutines.
+	for {
+		processUpdates(log)
+
+		amsterdamTime := date.Amsterdam()
+
+		// I found the docs quite confusing, but this is correct; see https://go.dev/play/p/iJ98dWNp6R7.
+		// TODO: Make this easier to test in dry run mode.
+		if amsterdamTime.Hour() == 15 && amsterdamTime.Minute() == 1 {
+			postMessage(log)
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+
+}
+
+func processUpdates(log *slog.Logger) error {
+	resp, err := doTelegramRequest(log, "getUpdates", url.Values{
+		"offset": {strconv.FormatUint(lastProcessedUpdateID+1, 10)},
+	})
+	if err != nil {
+		return err
+	}
+
+	updates := resp["result"].([]any)
+
+	for _, update := range updates {
+		updateID := uint64(update.(map[string]any)["update_id"].(float64))
+		userID := uint64(update.(map[string]any)["message"].(map[string]any)["from"].(map[string]any)["id"].(float64))
+		userIDString := strconv.FormatUint(userID, 10)
+		text, hasText := update.(map[string]any)["message"].(map[string]any)["text"].(string)
+
+		// This means any errors won't cause the bot to get stuck in a loop.
+		lastProcessedUpdateID = updateID
+
+		if !hasText {
+			_, err := doTelegramRequest(log, "sendMessage", url.Values{
+				"chat_id": {userIDString},
+				"text":    {"Sorry, ik begrijp je niet. Probeer /start of /privacy."},
+			})
+			if err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		switch text {
+		case "/start":
+			_, err := doTelegramRequest(log, "sendMessage", url.Values{
+				"chat_id": {userIDString},
+				"text":    {"Hallo! In privé-chats kan ik niet zo veel. Mijn kanaal @energieprijzen is veel interessanter. Je kunt via /privacy lezen hoe ik met je gegevens omga."},
+				// TODO: Add inline keyboard with "Privacy" and "Subscribe" buttons?
+			})
+			if err != nil {
+				return err
+			}
+		case "/privacy":
+			_, err := doTelegramRequest(log, "sendMessage", url.Values{
+				"chat_id": {userIDString},
+				// This way we can use Markdown code formatting in a raw string literal.
+				"text":       {strings.ReplaceAll(fmt.Sprintf(privacyPolicy, userID), "~", "`")},
+				"parse_mode": {"Markdown"},
+			})
+			if err != nil {
+				return err
+			}
+		default:
+			_, err := doTelegramRequest(log, "sendMessage", url.Values{
+				"chat_id": {userIDString},
+				"text":    {"Sorry, ik begrijp je niet. Probeer /start of /privacy."},
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func postMessage(log *slog.Logger) {
 	prices, err := internal.GetEnergyPrices(log)
 	if err != nil {
 		log.Error("could not get energy prices", slog.Any("err", err))
-		pingCronitor(log, stateFail)
 		os.Exit(1)
 	}
 
@@ -163,7 +230,6 @@ func main() {
 	err = report(data).Render(context.Background(), &sb)
 	if err != nil {
 		log.Error("could not render report", slog.Any("err", err))
-		pingCronitor(log, stateFail)
 		os.Exit(1)
 	}
 
@@ -178,7 +244,6 @@ func main() {
 	})
 	if err != nil {
 		log.Error("could not send message", slog.Any("err", err))
-		pingCronitor(log, stateFail)
 		os.Exit(1)
 	}
 
@@ -201,61 +266,33 @@ func main() {
 		idLogger.Info("message reacted to")
 	}
 
-	pingCronitor(log, stateComplete)
-}
-
-func pingCronitor(log *slog.Logger, s state) {
-	var state string
-
-	switch s {
-	case stateRun:
-		state = "run"
-	case stateComplete:
-		state = "complete"
-	case stateFail:
-		state = "fail"
-	default:
-		// Calling the function with an unknown state is a programming error, so let's fail loudly.
-		panic(fmt.Sprintf("unknown state: %d", s))
-	}
-
-	log = log.With(slog.String("state", state))
-
-	log.Info("pinging cronitor")
-
-	if dryRun {
-		return
-	}
-
-	if monitorURL == "" {
-		log.Warn("no cronitor URL set, not pinging cronitor")
-		return
-	}
-
-	resp, err := http.Get(fmt.Sprintf("%s?state=%s", monitorURL, state))
-	if err != nil {
-		log.Error("could not ping cronitor", slog.Any("err", err))
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		log.Error("unexpected status code while pinging cronitor", slog.Group("response", slog.Int("status_code", resp.StatusCode)))
-		os.Exit(1)
-	}
 }
 
 func doTelegramRequest(log *slog.Logger, method string, params url.Values) (map[string]any, error) {
-	log.Info("sending telegram request", slog.String("method", method), slog.Any("params", params))
+	// TODO: Maybe logging in this function is too noisy?
+
+	// Note that we don't log the params for privacy reasons.
+	log.Info("sending telegram request", slog.String("method", method))
 
 	if dryRun {
-		// This is just enough of a successful response to `sendMessage` to make the program work.
-		// Note that we aren't mocking setMessageReaction, but that's OK because we don't care about its response.
-		return map[string]any{
-			"result": map[string]any{
-				"message_id": float64(0),
-			},
-		}, nil
+		switch method {
+		case "sendMessage":
+			// This is just enough of a response to make the bot think it sent a message.
+			return map[string]any{
+				"result": map[string]any{
+					"message_id": float64(0),
+				},
+			}, nil
+		case "getUpdates":
+			return map[string]any{
+				"result": []any{},
+			}, nil
+		case "setMessageReaction":
+			// We don't have to mock a response because we never check it.
+			return nil, nil
+		default:
+			panic(fmt.Sprintf("unknown method: %s", method))
+		}
 	}
 
 	resp, err := http.PostForm(fmt.Sprintf("https://api.telegram.org/bot%s/%s", token, method), params)
@@ -269,7 +306,8 @@ func doTelegramRequest(log *slog.Logger, method string, params url.Values) (map[
 		return nil, fmt.Errorf("could not read response body: %w", err)
 	}
 
-	log.Info("response", slog.Group("response", slog.Int("status_code", resp.StatusCode), slog.String("body", string(body))))
+	// Note that we don't log the response body for privacy reasons.
+	log.Info("received response from telegram", slog.Group("response", slog.Int("status_code", resp.StatusCode)))
 
 	var m map[string]any
 
---
+++ cmd/bot/privacy_policy.go
@@ -0,0 +1,39 @@
+package main
+
+const privacyPolicy = `*Privacybeleid*
+
+Ingangsdatum: 6 juli 2024.
+
+1. *Opslag en delen van gegevens*: We slaan je gegevens niet permanent op en delen of verkopen ze niet.
+
+2. *Bewaren van gegevens*: 
+   • We gebruiken je Telegram gebruikers-ID om te reageren op je commando's. Je gebruikers-ID is nodig om te zorgen dat het bericht bij jou aankomt. Dit is iets anders dan je telefoonnummer (welke de bot niet kan zien).
+   • Het ID wordt vrijwel direct daarna (binnen enkele seconden) verwijderd.
+   • Er worden geen andere gegevens uit je Telegram-profiel gebruikt voor welk doel dan ook.
+
+3. *AVG-rechten*: Je hebt door de AVG het recht om je gegevens in te zien, te corrigeren of te verwijderen.
+   • *Inzien*: Dit is je Telegram gebruikers-ID: ~%d~. Zoals eerder gezegd wordt deze niet permanent opgeslagen.
+   • *Corrigeren*: Als je van mening bent dat je Telegram gebruikers-ID niet klopt, neem dan contact op met Telegram. De bot kan je daar helaas niet bij helpen.
+   • *Verwijderen*: Omdat de bot uit zichzelf geen gebruikersgegevens opslaat, kun je gewoon stoppen met het gebruiken van de bot of in het uiterste geval je Telegram-account verwijderen.
+
+4. *Afhandeling van berichten*: Berichten die de bot niet begrijpt (zoals die met foto's, bestanden, stickers, onbekende commando's, enz.) worden zonder verdere verwerking automatisch verwijderd wanneer de bot nieuwe berichten ophaalt.
+
+5. *Diensten van derden*: De bot deelt alleen gebruikersgegvens met Telegram. Wanneer je een bericht aan de bot richt, ontvangt de bot het bericht en publieke informatie over je profiel van Telegram, en ontvangt Telegram van de bot jouw gebruikers-ID en een antwoord op je bericht.
+
+6. *Privacy van kinderen*: De bot slaat noch van kinderen, noch van volwassenen gegevens op.
+
+7. *Veiligheidsmaatregelen*: 
+   • Onze servers zijn beveiligd met sterke wachtzinnen en firewalls. 
+   • Ze worden regelmatig bijgewerkt met de nieuwste patches. 
+   • We gebruiken HTTPS voor de communicatie met Telegram, wat privacy en veiligheid garandeert.
+
+8. *Beleidsupdates*: Eventuele wijzigingen in dit beleid zullen van tevoren worden aangekondigd via @energieprijzen. We streven ernaar om wijzigingen minstens 14 dagen van tevoren aan te kondigen. In bepaalde gevallen (bijvoorbeeld als er een urgente wijziging nodig is, als het beleid niet fundamenteel wordt gewijzigd, of als het beleid zo wordt aangepast dat gebruikers meer rechten krijgen), kan daarvan worden afgeweken. In dergelijke gevallen zullen wij gebruikers zo snel mogelijk informeren over de wijzigingen en waarom van de standaardwachttijd is afgeweken.
+
+9. *Voorrang*: In geval van tegenstrijdigheden hebben de volgende documenten voorrang op dit beleid:
+   • De servicevoorwaarden, het privacybeleid en de Bot Platform Developer Terms of Service van Telegram;
+   • Toepasselijke wet- en regelgeving;
+   • De richtlijnen voor ontwikkelaars van Apple en Google.
+
+*Contact*: Als je vragen of opmerkingen hebt, kun je in het Nederlands of Engels contact opnemen met @xenial.
+
+Bedankt voor het gebruiken van de bot.`
