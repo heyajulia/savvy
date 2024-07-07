788f3260c4a8be691a9c61fbe09ce482a162dc20
Julia <juliariety@gmail.com>
2024-07-07 12:38:08 +0000

    Update privacy policy.
    - Change "Ingangsdatum" to "Datum van inwerkingtreding."
    - Add "Laatste wijziging," the date the privacy policy was last updated.
    - Remove the suggestion to remove one's Telegram account, since stopping
    use of the bot is sufficient for it to "delete" the user's data, since
    it does not store any.
    - Make it clearer that no user data is shared with third parties other
    than Telegram itself.
    - Change "wachtzinnen" to "wachtwoordzinnen" in the section about
    security measures.
    - Reorder the list of documents that take precedence over the privacy
    policy in case of conflict.

--- cmd/bot/privacy_policy.go
+++ cmd/bot/privacy_policy.go
@@ -2,7 +2,8 @@ package main
 
 const privacyPolicy = `*Privacybeleid*
 
-Ingangsdatum: 6 juli 2024.
+*Datum van inwerkingtreding*: 6 juli 2024.
+*Laatste wijziging*: 7 juli 2024.
 
 1. *Opslag en delen van gegevens*: We slaan je gegevens niet permanent op en delen of verkopen ze niet.
 
@@ -14,26 +15,26 @@ Ingangsdatum: 6 juli 2024.
 3. *AVG-rechten*: Je hebt door de AVG het recht om je gegevens in te zien, te corrigeren of te verwijderen.
    • *Inzien*: Dit is je Telegram gebruikers-ID: ~%d~. Zoals eerder gezegd wordt deze niet permanent opgeslagen.
    • *Corrigeren*: Als je van mening bent dat je Telegram gebruikers-ID niet klopt, neem dan contact op met Telegram. De bot kan je daar helaas niet bij helpen.
-   • *Verwijderen*: Omdat de bot uit zichzelf geen gebruikersgegevens opslaat, kun je gewoon stoppen met het gebruiken van de bot of in het uiterste geval je Telegram-account verwijderen.
+   • *Verwijderen*: Omdat de bot uit zichzelf geen gebruikersgegevens opslaat, kun je gewoon stoppen met het gebruiken van de bot.
 
 4. *Afhandeling van berichten*: Berichten die de bot niet begrijpt (zoals die met foto's, bestanden, stickers, onbekende commando's, enz.) worden zonder verdere verwerking automatisch verwijderd wanneer de bot nieuwe berichten ophaalt.
 
-5. *Diensten van derden*: De bot deelt alleen gebruikersgegvens met Telegram. Wanneer je een bericht aan de bot richt, ontvangt de bot het bericht en publieke informatie over je profiel van Telegram, en ontvangt Telegram van de bot jouw gebruikers-ID en een antwoord op je bericht.
+5. *Diensten van derden*: De bot deelt alleen gebruikersgegvens met Telegram. Wanneer je een bericht aan de bot richt, ontvangt de bot het bericht en publieke informatie over je profiel van Telegram, en ontvangt Telegram van de bot jouw gebruikers-ID en een antwoord op je bericht. De bot deelt geen gebruikersgegevens met andere derden.
 
 6. *Privacy van kinderen*: De bot slaat noch van kinderen, noch van volwassenen gegevens op.
 
 7. *Veiligheidsmaatregelen*: 
-   • Onze servers zijn beveiligd met sterke wachtzinnen en firewalls. 
+   • Onze servers zijn beveiligd met sterke wachtwoordzinnen en firewalls. 
    • Ze worden regelmatig bijgewerkt met de nieuwste patches. 
    • We gebruiken HTTPS voor de communicatie met Telegram, wat privacy en veiligheid garandeert.
 
 8. *Beleidsupdates*: Eventuele wijzigingen in dit beleid zullen van tevoren worden aangekondigd via @energieprijzen. We streven ernaar om wijzigingen minstens 14 dagen van tevoren aan te kondigen. In bepaalde gevallen (bijvoorbeeld als er een urgente wijziging nodig is, als het beleid niet fundamenteel wordt gewijzigd, of als het beleid zo wordt aangepast dat gebruikers meer rechten krijgen), kan daarvan worden afgeweken. In dergelijke gevallen zullen wij gebruikers zo snel mogelijk informeren over de wijzigingen en waarom van de standaardwachttijd is afgeweken.
 
 9. *Voorrang*: In geval van tegenstrijdigheden hebben de volgende documenten voorrang op dit beleid:
-   • De servicevoorwaarden, het privacybeleid en de Bot Platform Developer Terms of Service van Telegram;
    • Toepasselijke wet- en regelgeving;
+   • De servicevoorwaarden, het privacybeleid en de Bot Platform Developer Terms of Service van Telegram;
    • De richtlijnen voor ontwikkelaars van Apple en Google.
 
-*Contact*: Als je vragen of opmerkingen hebt, kun je in het Nederlands of Engels contact opnemen met @xenial.
+10. *Contact*: Als je vragen of opmerkingen hebt, kun je in het Nederlands of Engels contact opnemen met @xenial.
 
 Bedankt voor het gebruiken van de bot.`
