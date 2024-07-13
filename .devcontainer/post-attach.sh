6a548871465be8bc4734ffd1b5b841feb40c381c
Julia <juliariety@gmail.com>
2024-07-13 18:41:27 +0000

    Refactor post-attach script.

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -2,19 +2,20 @@
 
 set -euxo pipefail
 
-if [ -d "/home/vscode/.cache" ]; then
-  # Thanks to https://go.dev/issue/42353#issuecomment-721913348
-  sudo chown vscode:vscode /home/vscode/.cache
-fi
+ensure_vscode_ownership() {
+  if [ -d "$1" ]; then
+    sudo chown vscode:vscode "$1"
+  fi
+}
 
-if [ -d "/IdeaProjects/energieprijzen" ]; then
-  sudo chown vscode:vscode /IdeaProjects/energieprijzen
-fi
+configure_git_safe_directory() {
+  if [ -d "$1" ]; then
+    git config --global --add safe.directory "$1"
+  fi
+}
 
-if [ -d "/IdeaProjects" ]; then
-  git config --global --add safe.directory /IdeaProjects
-fi
+ensure_vscode_ownership "/home/vscode/.cache" # Thanks to https://go.dev/issue/42353#issuecomment-721913348
+ensure_vscode_ownership "/IdeaProjects/energieprijzen"
 
-if [ -d "/workspaces/energieprijzen" ]; then
-  git config --global --add safe.directory /workspaces/energieprijzen
-fi
+configure_git_safe_directory "/IdeaProjects"
+configure_git_safe_directory "/workspaces/energieprijzen"
