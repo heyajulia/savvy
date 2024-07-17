8fcae842c0e2866efd5147064d9ebf114ecf61b2
Julia <juliariety@gmail.com>
2024-07-17 12:20:38 +0000

    Implement "encoding/json".Unmarshaler for chatID.
    This commit adds a custom UnmarshalJSON method to the chatID type to fix
    the "json: cannot unmarshal ... into Go struct field ..." error.

--- cmd/bot/configuration.go
+++ cmd/bot/configuration.go
@@ -1,6 +1,10 @@
 package main
 
-import "strconv"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
 
 type configuration struct {
 	Telegram telegram              `json:"telegram"`
@@ -17,19 +21,43 @@ type cronitorConfiguration struct {
 	TelemetryURL string `json:"telemetry_url"`
 }
 
-// TODO: Make these fields private and implement "encoding/json".Unmarshaler.
 type chatID struct {
-	ID       *uint64
-	Username *string
+	id       *uint64
+	username *string
 }
+
+var (
+	// Verify interface compliance.
+	_ json.Unmarshaler = (*chatID)(nil)
+	_ fmt.Stringer     = (*chatID)(nil)
+)
 
 func (c *chatID) String() string {
 	switch {
-	case c.ID != nil:
-		return strconv.FormatUint(*c.ID, 10)
-	case c.Username != nil:
-		return *c.Username
+	case c.id != nil:
+		return strconv.FormatUint(*c.id, 10)
+	case c.username != nil:
+		return *c.username
 	default:
 		return ""
 	}
+}
+
+func (c *chatID) UnmarshalJSON(data []byte) error {
+	// Try to unmarshal data as a number.
+	var id uint64
+	if err := json.Unmarshal(data, &id); err == nil {
+		c.id = &id
+		return nil
+	}
+
+	// Try to unmarshal data as a string.
+	var username string
+	if err := json.Unmarshal(data, &username); err == nil {
+		c.username = &username
+		return nil
+	}
+
+	// Return an error if data is neither a number nor a string.
+	return fmt.Errorf("chatID: cannot unmarshal %v", string(data))
 }
