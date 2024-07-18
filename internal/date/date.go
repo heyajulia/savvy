a07dab584d05bfcb22340d8fe8905e9e74d6fb78
Julia <juliariety@gmail.com>
2024-07-18 12:11:41 +0000

    Improve performance of date.Tomorrow function.
    By moving the creation of the strings.Replacer to the package level, we
    avoid creating a new replacer every time the Tomorrow function is
    called. This only saves kilobytes of memory and nanoseconds of CPU time
    per day, but the relative improvement in performance is significant.
    
    Before:
    
    BenchmarkTomorrow-8: 405926 ops, 2893 ns/op, 6960 B/op, 62 allocs/op
    
    After:
    
    BenchmarkTomorrow-8: 3968139 ops, 288.6 ns/op, 144 B/op, 5 allocs/op

--- internal/date/date.go
+++ internal/date/date.go
@@ -20,31 +20,31 @@ func Amsterdam() time.Time {
 	return time.Now().In(amsterdam)
 }
 
+var replacer = strings.NewReplacer(
+	"Monday", "maandag",
+	"Tuesday", "dinsdag",
+	"Wednesday", "woensdag",
+	"Thursday", "donderdag",
+	"Friday", "vrijdag",
+	"Saturday", "zaterdag",
+	"Sunday", "zondag",
+	"January", "januari",
+	"February", "februari",
+	"March", "maart",
+	"April", "april",
+	"May", "mei",
+	"June", "juni",
+	"July", "juli",
+	"August", "augustus",
+	"September", "september",
+	"October", "oktober",
+	"November", "november",
+	"December", "december",
+)
+
 func Tomorrow() string {
 	today := Amsterdam()
 	tomorrow := today.AddDate(0, 0, 1)
-
-	replacer := strings.NewReplacer(
-		"Monday", "maandag",
-		"Tuesday", "dinsdag",
-		"Wednesday", "woensdag",
-		"Thursday", "donderdag",
-		"Friday", "vrijdag",
-		"Saturday", "zaterdag",
-		"Sunday", "zondag",
-		"January", "januari",
-		"February", "februari",
-		"March", "maart",
-		"April", "april",
-		"May", "mei",
-		"June", "juni",
-		"July", "juli",
-		"August", "augustus",
-		"September", "september",
-		"October", "oktober",
-		"November", "november",
-		"December", "december",
-	)
 
 	return replacer.Replace(tomorrow.Format("Monday 2 January 2006"))
 }
