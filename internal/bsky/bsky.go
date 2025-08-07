d90e004ff8b64ca34a4de98c185ea94dadefdb7d
Julia <juliariety@gmail.com>
2025-08-07 15:45:30 +0200

    Refactor "internal/bsky" to use the official SDK.
    This makes the code cleaner, more readable and more type-safe.
    
    Some methods take a context.Context parameter, which we are currently
    not using. I also didn't want to change the internal-public API, but
    maybe we could get rid of the "internal/bsky" package altogether
    someday.

--- internal/bsky/bsky.go
+++ internal/bsky/bsky.go
@@ -1,45 +1,61 @@
 package bsky
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
 	"fmt"
-	"net/http"
 	"time"
 
+	comatproto "github.com/bluesky-social/indigo/api/atproto"
+	appbsky "github.com/bluesky-social/indigo/api/bsky"
+	"github.com/bluesky-social/indigo/atproto/identity"
 	"github.com/bluesky-social/indigo/atproto/syntax"
+	lexutil "github.com/bluesky-social/indigo/lex/util"
+	"github.com/bluesky-social/indigo/util"
+	"github.com/bluesky-social/indigo/xrpc"
 )
 
-type loginResponse struct {
-	AccessJwt string `json:"accessJwt"`
-	Did       string `json:"did"`
-	// There are more fields, but we don't care about those.
-}
-
 type client struct {
-	accessJwt, did string
+	client *xrpc.Client
 }
 
 func Login(username, password string) (*client, error) {
-	req, err := createRequest("com.atproto.server.createSession", map[string]string{
-		"identifier": username,
-		"password":   password,
+	xrpcc := &xrpc.Client{
+		Client: util.RobustHTTPClient(),
+		Host:   "https://bsky.social",
+		Auth:   &xrpc.AuthInfo{Handle: username},
+	}
+
+	// TODO: Handle contexts better?
+
+	auth, err := comatproto.ServerCreateSession(context.Background(), xrpcc, &comatproto.ServerCreateSession_Input{
+		Identifier: xrpcc.Auth.Handle,
+		Password:   password,
 	})
 	if err != nil {
-		return nil, fmt.Errorf("bsky: login request: %w", err)
+		return nil, fmt.Errorf("bsky: create session: %w", err)
 	}
 
-	csr, err := sendRequest[loginResponse](req)
-	if err != nil {
-		return nil, fmt.Errorf("bsky: send login request: %w", err)
+	if auth.DidDoc == nil {
+		return nil, fmt.Errorf("bsky: no DID document in auth response")
 	}
 
-	client := &client{
-		accessJwt: csr.AccessJwt,
-		did:       csr.Did,
+	xrpcc.Host = pds((*auth.DidDoc).(identity.DIDDocument).Service)
+	xrpcc.Auth.AccessJwt = auth.AccessJwt
+	xrpcc.Auth.RefreshJwt = auth.RefreshJwt
+	xrpcc.Auth.Did = auth.Did
+	xrpcc.Auth.Handle = auth.Handle
+
+	return &client{client: xrpcc}, nil
+}
+
+func pds(services []identity.DocService) string {
+	for _, service := range services {
+		if service.Type == "AtprotoPersonalDataServer" {
+			return service.ServiceEndpoint
+		}
 	}
 
-	return client, nil
+	panic("bsky: no PDS found in DID document services")
 }
 
 func (c *client) Post(summary, telegramUrl string) error {
@@ -48,97 +64,58 @@ func (c *client) Post(summary, telegramUrl string) error {
 	text := fmt.Sprintf("%s\n\n%s", summary, anchorText)
 
 	t := time.Now().UTC()
-	rkey := syntax.NewTID(t.UnixMicro(), 42)
+	ts := t.Format(time.RFC3339)
+	rkey := string(syntax.NewTID(t.UnixMicro(), 42))
 
-	req, err := createRequest("com.atproto.repo.applyWrites", map[string]any{
-		"repo": c.did,
-		"writes": []map[string]any{
+	if _, err := comatproto.RepoApplyWrites(context.Background(), c.client, &comatproto.RepoApplyWrites_Input{
+		Repo: c.client.Auth.Did,
+		Writes: []*comatproto.RepoApplyWrites_Input_Writes_Elem{
 			{
-				"$type":      "com.atproto.repo.applyWrites#create",
-				"collection": "app.bsky.feed.post",
-				"rkey":       rkey,
-				"value": map[string]any{
-					"$type":     "app.bsky.feed.post",
-					"text":      text,
-					"createdAt": t,
-					"langs":     []string{"nl"},
-					"facets": []map[string]any{
-						{
-							"index": map[string]int{
-								"byteStart": len(text) - len(anchorText),
-								"byteEnd":   len(text),
-							},
-							"features": []map[string]string{
+				RepoApplyWrites_Create: &comatproto.RepoApplyWrites_Create{
+					Collection: "app.bsky.feed.post",
+					Rkey:       &rkey,
+					Value: &lexutil.LexiconTypeDecoder{
+						Val: &appbsky.FeedPost{
+							CreatedAt: ts,
+							Facets: []*appbsky.RichtextFacet{
 								{
-									"$type": "app.bsky.richtext.facet#link",
-									"uri":   telegramUrl,
+									Index: &appbsky.RichtextFacet_ByteSlice{
+										ByteStart: int64(len(text) - len(anchorText)),
+										ByteEnd:   int64(len(text)),
+									},
+									Features: []*appbsky.RichtextFacet_Features_Elem{
+										{
+											RichtextFacet_Link: &appbsky.RichtextFacet_Link{
+												Uri: telegramUrl,
+											},
+										},
+									},
 								},
 							},
+							Langs: []string{"nl"},
+							Tags:  []string{"energie", "energiebericht", "groen", "duurzaam", "stroom", "klimaat", "klimaatverandering"},
+							Text:  text,
 						},
 					},
 				},
 			},
 			{
-				"$type":      "com.atproto.repo.applyWrites#create",
-				"collection": "app.bsky.feed.threadgate",
-				"rkey":       rkey,
-				"value": map[string]any{
-					"$type":     "app.bsky.feed.threadgate",
-					"post":      fmt.Sprintf("at://%s/app.bsky.feed.post/%s", c.did, rkey),
-					"allow":     []map[string]string{},
-					"createdAt": t,
+				RepoApplyWrites_Create: &comatproto.RepoApplyWrites_Create{
+					Collection: "app.bsky.feed.threadgate",
+					Rkey:       &rkey,
+					Value: &lexutil.LexiconTypeDecoder{
+						Val: &appbsky.FeedThreadgate{
+							Allow:     []*appbsky.FeedThreadgate_Allow_Elem{},
+							CreatedAt: ts,
+							Post:      fmt.Sprintf("at://%s/app.bsky.feed.post/%s", c.client.Auth.Did, rkey),
+						},
+					},
 				},
 			},
 		},
-	})
-	if err != nil {
-		return fmt.Errorf("bsky: post and threadgate request: %w", err)
-	}
-
-	if _, err := sendRequest[any](req, c.accessJwt); err != nil {
-		return fmt.Errorf("bsky: send post and threadgate request: %w", err)
+	}); err != nil {
+		return fmt.Errorf("bsky: apply writes: %w", err)
 	}
 
 	return nil
-}
-
-func createRequest[TRequest any](method string, data TRequest) (*http.Request, error) {
-	payloadBytes, err := json.Marshal(data)
-	if err != nil {
-		return nil, fmt.Errorf("encode request: %w", err)
-	}
-	body := bytes.NewReader(payloadBytes)
-
-	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://bsky.social/xrpc/%s", method), body)
-	if err != nil {
-		return nil, fmt.Errorf("create request: %w", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	return req, nil
-}
-
-func sendRequest[TResponse any](req *http.Request, accessJwt ...string) (*TResponse, error) {
-	if len(accessJwt) == 1 {
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessJwt[0]))
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unexpected status code %d", resp.StatusCode)
-	}
-
-	var r TResponse
-
-	err = json.NewDecoder(resp.Body).Decode(&r)
-	if err != nil {
-		return nil, fmt.Errorf("decode response: %w", err)
-	}
-
-	return &r, nil
 }
