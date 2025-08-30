79d5de67991425a65bf8ec68043d07f27e1c4d4d
Julia <juliariety@gmail.com>
2025-08-30 12:49:04 +0200

    Add ResolveMiniDoc function to "pkg/discoverpds".

--- pkg/discoverpds/pds.go
+++ pkg/discoverpds/pds.go
@@ -1,4 +1,4 @@
-// Package discoverpds finds the Personal Data Server for a given AT Protocol handle.
+// Package discoverpds finds the Personal Data Server for a given AT Protocol identifier.
 package discoverpds
 
 import (
@@ -19,11 +19,21 @@ type miniDocError struct {
 	Message string `json:"message"`
 }
 
-// PDS finds the Personal Data Server for the given AT Protocol handle or DID.
+// PDS finds the Personal Data Server for the given AT Protocol identifier.
 func PDS(identifier string) (string, error) {
+	doc, err := ResolveMiniDoc(identifier)
+	if err != nil {
+		return "", fmt.Errorf("resolve mini doc for %q: %w", identifier, err)
+	}
+
+	return doc.PDS, nil
+}
+
+// ResolveMiniDoc resolves the mini document for the given AT Protocol identifier.
+func ResolveMiniDoc(identifier string) (*miniDocResponse, error) {
 	req, err := http.NewRequest(http.MethodGet, "https://slingshot.microcosm.blue/xrpc/com.bad-example.identity.resolveMiniDoc", nil)
 	if err != nil {
-		return "", fmt.Errorf("resolve DID %q: %w", identifier, err)
+		return nil, fmt.Errorf("resolve DID %q: %w", identifier, err)
 	}
 
 	q := req.URL.Query()
@@ -32,7 +42,7 @@ func PDS(identifier string) (string, error) {
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("resolve DID %q: %w", identifier, err)
+		return nil, fmt.Errorf("resolve DID %q: %w", identifier, err)
 	}
 	defer resp.Body.Close()
 
@@ -41,19 +51,19 @@ func PDS(identifier string) (string, error) {
 		var doc miniDocResponse
 
 		if err := json.NewDecoder(resp.Body).Decode(&doc); err != nil {
-			return "", fmt.Errorf("decode DID document response: %w", err)
+			return nil, fmt.Errorf("decode DID document response: %w", err)
 		}
 
-		return doc.PDS, nil
+		return &doc, nil
 	case http.StatusBadRequest:
 		var doc miniDocError
 
 		if err := json.NewDecoder(resp.Body).Decode(&doc); err != nil {
-			return "", fmt.Errorf("decode error response: %w", err)
+			return nil, fmt.Errorf("decode error response: %w", err)
 		}
 
-		return "", fmt.Errorf("bad request: %s: %s", doc.Error, doc.Message)
+		return nil, fmt.Errorf("bad request: %s: %s", doc.Error, doc.Message)
 	default:
-		return "", fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 }
